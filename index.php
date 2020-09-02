3b5d1ac5e0cff3f6508168925d24b04457ec0848
s-bhageloe <2117940@talnet.nl>
2020-09-02 15:40:50 +0200

    Commiting index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+// Opdr.1
+/*
+
+$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+
+
+ print_r($klas);
+
+// Opdr.2
+for ($i=0; $i <count($klas ) ; $i++) { 
+	echo $klas[$i].PHP_EOL;
+}
+
+//Opdr.3
+
+foreach ($klas as $namen) {
+	echo $namen.PHP_EOL;
+	
+}
+
+
+// Combinatie opdracht
+$bucket_list = [];
+
+$hoeveelheid_activiteiten = readline("Hoeveel activiteiten wil je toevoegen aan je bucket list?");
+
+for ($i=0; $i < $hoeveelheid_activiteiten ; $i++) { 
+    $activiteit = readline("Welke activiteiten wil je toevoegen?");
+    array_push($bucket_list, $activiteit);
+
+}
+
+print_r($bucket_list);
+
+foreach ($bucket_list as $combineren) {
+    echo($combineren.PHP_EOL);
+
+}
+*/
+
+?>
